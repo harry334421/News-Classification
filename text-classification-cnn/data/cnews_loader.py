98c7a6ea73e4493a02053924aada8be85c6ac0a9
harry334421 <58018547+harry334421@users.noreply.github.com>
2019-12-01 20:01:02 +0800

    Update cnews_loader.py

--- text-classification-cnn/data/cnews_loader.py
+++ text-classification-cnn/data/cnews_loader.py
@@ -6,19 +6,8 @@ from collections import Counter
 import numpy as np
 import tensorflow.contrib.keras as kr
 
-if sys.version_info[0] > 2:
-    is_py3 = True
-else:
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
-    is_py3 = False
-
 
 def native_word(word, encoding='utf-8'):
-    """如果在python2下面使用python3训练的模型，可考虑调用此函数转化一下字符编码"""
-    if not is_py3:
-        return word.encode(encoding)
-    else:
         return word
 
 
@@ -30,14 +19,7 @@ def native_content(content):
 
 
 def open_file(filename, mode='r'):
-    """
-    常用文件操作，可在python2和python3间切换.
-    mode: 'r' or 'w' for read or write
-    """
-    if is_py3:
         return open(filename, mode, encoding='utf-8', errors='ignore')
-    else:
-        return open(filename, mode)
 
 
 def read_file(filename):
@@ -66,7 +48,6 @@ def build_vocab(train_dir, vocab_dir, vocab_size=5000):
     counter = Counter(all_data)
     count_pairs = counter.most_common(vocab_size - 1)
     words, _ = list(zip(*count_pairs))
-    # 添加一个 <PAD> 来将所有文本pad为同一长度
     words = ['<PAD>'] + list(words)
     open_file(vocab_dir, mode='w').write('\n'.join(words) + '\n')
 
@@ -75,14 +56,12 @@ def read_vocab(vocab_dir):
     """读取词汇表"""
     # words = open_file(vocab_dir).read().strip().split('\n')
     with open_file(vocab_dir) as fp:
-        # 如果是py2 则每个值都转化为unicode
         words = [native_content(_.strip()) for _ in fp.readlines()]
     word_to_id = dict(zip(words, range(len(words))))
     return words, word_to_id
 
 
 def read_category():
-    """读取分类目录，固定"""
     categories = ['体育', '财经', '房产', '家居', '教育', '科技', '时尚', '时政', '游戏', '娱乐']
 
     categories = [native_content(x) for x in categories]
@@ -98,7 +77,7 @@ def to_words(content, words):
 
 
 def process_file(filename, word_to_id, cat_to_id, max_length=600):
-    """将文件转换为id表示"""
+
     contents, labels = read_file(filename)
 
     data_id, label_id = [], []
@@ -106,9 +85,8 @@ def process_file(filename, word_to_id, cat_to_id, max_length=600):
         data_id.append([word_to_id[x] for x in contents[i] if x in word_to_id])
         label_id.append(cat_to_id[labels[i]])
 
-    # 使用keras提供的pad_sequences来将文本pad为固定长度
     x_pad = kr.preprocessing.sequence.pad_sequences(data_id, max_length)
-    y_pad = kr.utils.to_categorical(label_id, num_classes=len(cat_to_id))  # 将标签转换为one-hot表示
+    y_pad = kr.utils.to_categorical(label_id, num_classes=len(cat_to_id))  
 
     return x_pad, y_pad
 
